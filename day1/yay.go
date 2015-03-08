7d6fa431af4ec6d44a7594e5be3a155284589558
Katy Blumer <katyblumer@gmail.com>
2015-03-07 20:42:37 -0800

    1D Nonlinear Convection - zero before boundary

--- day1/yay.go
+++ day1/yay.go
@@ -13,22 +13,25 @@ type SimConstants struct {
 func main() {
 	
 	size := 41
-	t := 1.0
+	// t := 1.0
 	
-	maxIntensity := float64(10.0)
+	maxIntensity := float64(2.0)
 
 	const width = float64(2)
 	consts := SimConstants {
 		c: float64(1.0), 
-		tstep: float64(0.005), 
-		xstep: float64(width / float64(size)),
+		tstep: float64(0.025), 
+		xstep: float64(width / float64(size-1)),
 	}
-	runs := int(t / consts.tstep)
+	runs := 20//int(t / consts.tstep)
 
 	graph := image.NewRGBA(image.Rect(0, 0, runs, size))
 	a := make([]float64, size)
 	next := make([]float64, size)
 
+	for i := 0; i < size; i++ {
+		a[i] = 1.0
+	}
 	for i := 15; i < 26; i++ {
 		a[i] = maxIntensity
 	}
@@ -40,11 +43,12 @@ func main() {
 			color := createColor(a[j], maxIntensity)
 			graph.Set(i, j, color)
 		}
-		fmt.Println(a)
 		//make next row
 		update(a[:], next[:], consts)
 		a, next = next, a
+		fmt.Println(sum(&a))
 	}
+	
 
 	save(graph, "new.png")
 	show("new.png")
@@ -52,18 +56,18 @@ func main() {
 }
 
 func update(prev []float64, next []float64, consts SimConstants) {
-	ratio := (consts.c * consts.tstep / consts.xstep)
+	ratio := consts.tstep / consts.xstep
 	size := len(prev)
 	// first box
-	next[0] = prev[0] - ratio * (prev[0])
+	next[0] = prev[0] - ratio * prev[0] * prev[0]
 	for i := 1; i < size; i++ {
-		next[i] = prev[i] - ratio * (prev[i] - prev[i-1])
+		next[i] = prev[i] - ratio * prev[i] * (prev[i] - prev[i-1])
 	}
 	return
 }
 
 func createColor(point float64, maxIntensity float64) (ans color.Color) {
 	intensity := uint8(point  * (255/maxIntensity))
-	ans = color.RGBA{0, intensity, 0, 255}
+	ans = color.RGBA{intensity, intensity, intensity, 255}
 	return
 }

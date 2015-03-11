4e6f8f589b7f9f5fef2662ed7506e21c588c8951
Katy Blumer <katyblumer@gmail.com>
2015-03-11 08:16:56 -0700

    Added link to tutorial

--- day1/yay.go
+++ day1/yay.go
@@ -1,30 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
-	"fmt"
 )
+
+// heavily borrowed from: http://nbviewer.ipython.org/github/barbagroup/CFDPython/tree/master/lessons/
 
 type SimConstants struct {
 	baseIntensity, c, tstep, xstep float64
 }
 
 func main() {
-	
+
 	size := 41
 	// t := 1.0
-	
+
 	maxIntensity := float64(2.0)
 
 	const width = float64(2)
-	consts := SimConstants {
+	consts := SimConstants{
 		baseIntensity: 1.0,
-		c: float64(1.0), 
-		tstep: float64(0.025), 
-		xstep: float64(width / float64(size-1)),
+		c:             float64(1.0),
+		tstep:         float64(0.025),
+		xstep:         float64(width / float64(size-1)),
 	}
-	runs := 20//int(t / consts.tstep)
+	runs := 20 //int(t / consts.tstep)
 
 	graph := image.NewRGBA(image.Rect(0, 0, runs, size))
 	a := make([]float64, size)
@@ -36,9 +38,9 @@ func main() {
 	for i := 15; i < 26; i++ {
 		a[i] = maxIntensity
 	}
-	
-	for i := 0; i < runs; i++ {		
-		
+
+	for i := 0; i < runs; i++ {
+
 		// draw row
 		for j := 0; j < len(a); j++ {
 			color := createColor(a[j], maxIntensity)
@@ -49,26 +51,25 @@ func main() {
 		a, next = next, a
 		fmt.Println(sum(&a))
 	}
-	
 
 	save(graph, "new.png")
 	show("new.png")
-	
+
 }
 
 func update(prev []float64, next []float64, consts SimConstants) {
 	ratio := consts.tstep / consts.xstep
 	size := len(prev)
 	// first box
-	next[0] = prev[0] - ratio * prev[0] * (prev[0] - consts.baseIntensity)
+	next[0] = prev[0] - ratio*prev[0]*(prev[0]-consts.baseIntensity)
 	for i := 1; i < size; i++ {
-		next[i] = prev[i] - ratio * prev[i] * (prev[i] - prev[i-1])
+		next[i] = prev[i] - ratio*prev[i]*(prev[i]-prev[i-1])
 	}
 	return
 }
 
 func createColor(point float64, maxIntensity float64) (ans color.Color) {
-	intensity := uint8(point  * (255/maxIntensity))
+	intensity := uint8(point * (255 / maxIntensity))
 	ans = color.RGBA{intensity, intensity, intensity, 255}
 	return
 }

8a84615ce52d907429e68a39f332c7be0796f801
Katy Blumer <katyblumer@gmail.com>
2015-04-17 18:26:20 -0700

    clean up code

--- day2/yay.go
+++ day2/yay.go
@@ -10,46 +10,50 @@ import (
 
 type SimConstants struct {
 	baseIntensity, c, tstep, xstep float64
+	numSteps, numBoxes             int
+}
+
+type ViewConstants struct {
+	maxIntensity float64
 }
 
 func main() {
 
-	size := 41
 	// t := 1.0
+	// physicalWidth = float64(2.0)
+	// runs := 20 //int(t / simConsts.tstep)
 
-	maxIntensity := float64(2.0)
-
-	const width = float64(2)
-	consts := SimConstants{
+	simConsts := SimConstants{
+		numSteps:      20,
+		numBoxes:      41,
 		baseIntensity: 1.0,
 		c:             float64(1.0),
 		tstep:         float64(0.025),
-		xstep:         float64(width / float64(size-1)),
 	}
-	runs := 20 //int(t / consts.tstep)
+	simConsts.xstep = float64(2.0 / float64(simConsts.numBoxes-1))
 
-	graph := image.NewRGBA(image.Rect(0, 0, runs, size))
-	a := make([]float64, size)
-	next := make([]float64, size)
+	viewConsts := ViewConstants{maxIntensity: 2.0}
 
-	for i := 0; i < size; i++ {
-		a[i] = consts.baseIntensity
+	width := simConsts.numSteps
+	height := simConsts.numBoxes
+
+	graph := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	currRow := make([]float64, simConsts.numBoxes)
+	nextRow := make([]float64, simConsts.numBoxes)
+
+	for i := 0; i < simConsts.numBoxes; i++ {
+		currRow[i] = simConsts.baseIntensity
 	}
 	for i := 15; i < 26; i++ {
-		a[i] = maxIntensity
+		currRow[i] = viewConsts.maxIntensity
 	}
 
-	for i := 0; i < runs; i++ {
-
-		// draw row
-		for j := 0; j < len(a); j++ {
-			color := createColor(a[j], maxIntensity)
-			graph.Set(i, j, color)
-		}
-		//make next row
-		update(a[:], next[:], consts)
-		a, next = next, a
-		fmt.Println(sum(&a))
+	for i := 0; i < simConsts.numSteps; i++ {
+		drawRow(i, currRow[:], graph, viewConsts)
+		nextTimeStep(currRow[:], nextRow[:], simConsts)
+		fmt.Println(sum(&currRow))
+		currRow, nextRow = nextRow, currRow
 	}
 
 	save(graph, "new.png")
@@ -57,19 +61,26 @@ func main() {
 
 }
 
-func update(prev []float64, next []float64, consts SimConstants) {
-	ratio := consts.tstep / consts.xstep
+func nextTimeStep(prev []float64, next []float64, simConsts SimConstants) {
+	ratio := simConsts.tstep / simConsts.xstep
 	size := len(prev)
 	// first box
-	next[0] = prev[0] - ratio*prev[0]*(prev[0]-consts.baseIntensity)
+	next[0] = prev[0] - ratio*prev[0]*(prev[0]-simConsts.baseIntensity)
 	for i := 1; i < size; i++ {
 		next[i] = prev[i] - ratio*prev[i]*(prev[i]-prev[i-1])
 	}
 	return
 }
 
-func createColor(point float64, maxIntensity float64) (ans color.Color) {
-	intensity := uint8(point * (255 / maxIntensity))
+func drawRow(rowNum int, row []float64, graph *image.RGBA, viewConsts ViewConstants) {
+	for j := 0; j < len(row); j++ {
+		color := createColor(row[j], viewConsts.maxIntensity)
+		graph.Set(rowNum, j, color)
+	}
+}
+
+func createColor(pointIntensity float64, maxIntensity float64) (ans color.Color) {
+	intensity := uint8(pointIntensity * (255 / maxIntensity))
 	ans = color.RGBA{intensity, intensity, intensity, 255}
 	return
 }
